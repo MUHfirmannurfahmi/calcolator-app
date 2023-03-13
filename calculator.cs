43d116cf532dc32ff1d1a476e96201c7dd887803
MUHfirmannurfahmi <fhmiheluput33@gmail.com>
2023-03-13 16:45:44 +0700

    menambahkan fungsi penambahan calculator

--- calculator.cs
+++ calculator.cs
@@ -1,1 +1,11 @@
-#include <iostream>
+git remote add calculator https://github.com/MUHfirmannurfahmi/calcolator-app.git
+
+class calculator 
+{
+	static void main(string [] args)
+	{
+	}
+	static int penambahan (int a, int b ) {
+		return a + b ;
+	}
+}
